cb9d98504ee8de21834ae28cbf5d87b69932bedc
lucaswerner90 <wernerlucas12@gmail.com>
2018-03-18 11:17:41 +0100

    Fixed path on executable

--- bin/lib/autochromedriver.js
+++ bin/lib/autochromedriver.js
@@ -3,7 +3,7 @@
 const join = require('path').join;
 const spawn = require('child_process').spawn;
 const chmodSync = require('fs').chmodSync;
-const FILE_PATH = process.platform === 'win32' ? join(process.cwd(), 'lib', 'chromedriver.exe') : join(process.cwd(), 'lib', 'chromedriver');
+const FILE_PATH = process.platform === 'win32' ? join(__dirname, '..', 'lib', 'chromedriver.exe') : join(__dirname, 'lib', 'chromedriver');
 chmodSync(FILE_PATH, '755');
 const command = spawn(FILE_PATH);
 command.stdout.on('data', (data) => {
